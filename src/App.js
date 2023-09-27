fbd392327a1e113695a1c1bac067d0548575a4fb
EfrainIgnacio <126102694+EfrainIgnacio@users.noreply.github.com>
2023-09-26 19:08:16 -0600

    Frist Comit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+
+  // const axios = require('axios');
+
+    const url = 'https://demomatic.alquimiadigital.mx/cpanel/index.php/api/oauth2/token';
+    const data = 'grant_type=password&username=ejemplpo%40alquimiapay.com&password=Tupassword&client_id=testclient&client_secret=testpass';
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    };
+
+    axios.post(url, data, { headers })
+      .then((response) => {
+        console.log(response.data);
+        debugger
+      })
+      .catch((error) => {
+        console.error(error);
+        debugger
+      });
+
   return (
     <div className="App">
       <header className="App-header">
